473746eb83c0f373ab2d8129c4a024c8b310565e
Marcus441 <psminchia4153@hotmail.com>
2026-03-24 14:50:40 +1000

    refactor(value): added enum class for operations
    - added a MAKE_VALUE preprocessor definition to set the label of the
    value to the variable name
    - operations now set the m_op field to the enum value

--- include/value.h
+++ include/value.h
@@ -2,8 +2,19 @@
 
 #include <initializer_list>
 #include <memory>
-#include <ostream>
+#include <string>
 #include <vector>
+
+#define MAKE_VALUE(name, val)                                                  \
+  Value name{val};                                                             \
+  name.label(#name);
+
+enum class Operation {
+  ADD,
+  MULTIPLY,
+  TANH,
+  NONE,
+};
 
 class Value {
 public:
@@ -18,16 +29,30 @@ public:
   friend Value operator*(const Value &lhs, const Value &rhs);
 
   // Accessors
+  const std::string &label() const { return m_label; }
   double data() const { return m_state->data; }
+  double grad() const { return m_state->grad; }
   std::vector<Value> prev() const { return m_state->prev; }
+  Operation op() const { return m_op; }
+  const void *id() const { return m_state.get(); }
+
+  // Setters
+  void label(std::string label) { m_label = std::move(label); }
+
+  // Math
+  Value tanh();
 
 private:
   struct State {
+    double grad{0.0};
     double data;
     std::vector<Value> prev;
     State(double d, std::vector<Value> p = {}) : data(d), prev(p) {};
   };
   std::shared_ptr<State> m_state;
+  Operation m_op = Operation::NONE;
+  std::string m_label;
 };
+
 // Export formatting rules
 #include "formatting.h" // IWYU pragma: keep
--- src/value.cpp
+++ src/value.cpp
@@ -1,11 +1,22 @@
 #include "value.h"
+#include <cmath>
 
 Value operator+(const Value &lhs, const Value &rhs) {
   Value result(lhs.m_state->data + rhs.m_state->data, {lhs, rhs});
+  result.m_op = Operation::ADD;
   return result;
 }
 
 Value operator*(const Value &lhs, const Value &rhs) {
   Value result(lhs.m_state->data * rhs.m_state->data, {lhs, rhs});
+  result.m_op = Operation::MULTIPLY;
+  return result;
+}
+
+Value Value::tanh() {
+  double x = this->data();
+  double tanh_x = (std::exp(2 * x) - 1) / (std::exp(2 * x) + 1);
+  Value result = Value(tanh_x, {*this});
+  result.m_op = Operation::TANH;
   return result;
 }
