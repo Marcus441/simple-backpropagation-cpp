f259a7827016e1c5349d8ca905780361c41d5423
Marcus441 <psminchia4153@hotmail.com>
2026-04-01 21:09:08 +1000

    refactor(main): remove redundant macro calls

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,35 @@
 
 int main() {
   // inputs
-  MAKE_VALUE(x1, 2.0)
-  MAKE_VALUE(x2, 0.0)
+  Value x1{2.0};
+  x1.label("x1");
+
+  Value x2{0.0};
+  x2.label("x2");
 
   // weights
-  MAKE_VALUE(w1, -3.0)
-  MAKE_VALUE(w2, 1.0)
+  Value w1{-3.0};
+  w1.label("w1");
+  Value w2{1.0};
+  w2.label("w2");
 
   // bias
-  MAKE_VALUE(b, 6.8813735870195432)
+  Value b{6.8813735870195432};
+  b.label("b");
 
-  MAKE_VALUE(x1w1, x1 * w1)
-  MAKE_VALUE(x2w2, x2 * w2)
+  Value x1w1{x1 * w1};
+  x1w1.label("x1 * w1");
 
-  MAKE_VALUE(x1w1x2w2, x1w1 + x2w2)
-  MAKE_VALUE(n, x1w1x2w2 + b)
+  Value x2w2{x2 * w2};
+  x2w2.label("x2 * w2");
 
-  MAKE_VALUE(o, n.tanh())
+  Value x1w1x2w2{x1w1 + x2w2};
+  x1w1x2w2.label("x1*w1 + x2*w2");
+  Value n{x1w1x2w2 + b};
+  n.label("n");
 
-  std::print("{}: {}", o.label(), o.data());
+  Value o{n.tanh()};
+  o.backward();
 
   util::graphing::export_to_dot(o, "file.dot");
   std::println("");
