d034e3f8c698e4113b84b4a27690414b5e35cd1d
Marcus441 <psminchia4153@hotmail.com>
2026-04-11 11:34:59 +1000

    refactor(value): inline backward lambdas into SetBackward calls
    - Remove named backward variables and pass lambdas directly
    - Rename result to out for consistency across all operators

--- src/value.cpp
+++ src/value.cpp
@@ -32,14 +32,13 @@ auto operator/(const Value& lhs, const Value& rhs) -> Value {
   Value out(l_data / r_data, {lhs, rhs});
   out.m_state_->op_ = Operation::kDivide;
 
-  std::function<void(double)> backward = [lhs, rhs](double out_grad) -> void {
+  out.SetBackward([lhs, rhs](double out_grad) -> void {
     double l = lhs.Data();
     double r = rhs.Data();
     // Quotient Rule: d/dl (l/r) = 1/r  |  d/dr (l/r) = -l / r^2
     lhs.GradRef() += (1.0 / r) * out_grad;
     rhs.GradRef() += (-l / (r * r)) * out_grad;
-  };
-  out.SetBackward(backward);
+  });
 
   return out;
 }
@@ -56,72 +55,64 @@ auto operator-(const Value& lhs, const Value& rhs) -> Value {
 }
 
 auto operator+(const Value& lhs, const Value& rhs) -> Value {
-  Value result(lhs.m_state_->data_ + rhs.m_state_->data_, {lhs, rhs});
-  result.m_state_->op_ = Operation::kAdd;
+  Value out(lhs.m_state_->data_ + rhs.m_state_->data_, {lhs, rhs});
+  out.m_state_->op_ = Operation::kAdd;
 
-  std::function<void(double)> backward = [lhs, rhs](double out_grad) -> void {
+  out.SetBackward([lhs, rhs](double out_grad) -> void {
     lhs.GradRef() += 1.0F * out_grad;
     rhs.GradRef() += 1.0F * out_grad;
-  };
-  result.SetBackward(backward);
-  return result;
+  });
+  return out;
 }
 
 auto operator*(const Value& lhs, const Value& rhs) -> Value {
-  Value result(lhs.m_state_->data_ * rhs.m_state_->data_, {lhs, rhs});
-  result.m_state_->op_ = Operation::kMultiply;
+  Value out(lhs.m_state_->data_ * rhs.m_state_->data_, {lhs, rhs});
+  out.m_state_->op_ = Operation::kMultiply;
 
-  std::function<void(double)> backward = [lhs, rhs](double out_grad) -> void {
+  out.SetBackward([lhs, rhs](double out_grad) -> void {
     lhs.GradRef() += rhs.Data() * out_grad;
     rhs.GradRef() += lhs.Data() * out_grad;
-  };
-  result.SetBackward(backward);
-  return result;
+  });
+  return out;
 }
 
 auto Value::Tanh() -> Value {
   double x = this->Data();
   double tanh_x = std::tanh(x);
-  Value result = Value(tanh_x, {*this});
-  result.m_state_->op_ = Operation::kTanh;
+  Value out = Value(tanh_x, {*this});
+  out.m_state_->op_ = Operation::kTanh;
 
-  std::function<void(double)> backward = [*this, tanh_x](double out_grad) -> void {
+  out.SetBackward([*this, tanh_x](double out_grad) -> void {
     this->GradRef() += (1.0 - std::pow(tanh_x, 2)) * out_grad;
-  };
-  result.SetBackward(backward);
-  return result;
+  });
+  return out;
 }
 
 auto Value::Pow(double other) const -> Value {
   Value out(std::pow(this->Data(), other), {*this});
   out.m_state_->op_ = Operation::kPower;
-  std::function<void(double)> backward = [*this, other](double out_grad) -> void {
+
+  out.SetBackward([*this, other](double out_grad) -> void {
     this->GradRef() += (other * std::pow(this->Data(), other - 1)) * out_grad;
-  };
-  out.SetBackward(backward);
+  });
   return out;
 }
 
 auto Value::Exp() -> Value {
   double x = this->Data();
   double e = std::exp(x);
-  Value result(e, {*this});
-  result.m_state_->op_ = Operation::kExp;
-  std::function<void(double)> backward = [*this, e](double out_grad) -> void {
-    this->GradRef() += e * out_grad;
-  };
+  Value out(e, {*this});
+  out.m_state_->op_ = Operation::kExp;
 
-  result.SetBackward(backward);
-  return result;
+  out.SetBackward([*this, e](double out_grad) -> void { this->GradRef() += e * out_grad; });
+
+  return out;
 }
 
 auto operator*(double lhs, const Value& rhs) -> Value {
   Value out(lhs * rhs.Data(), {rhs});
   out.m_state_->op_ = Operation::kMultiply;
-  std::function<void(double)> backward = [lhs, rhs](double out_grad) -> void {
-    rhs.GradRef() += lhs * out_grad;
-  };
-  out.SetBackward(backward);
+  out.SetBackward([lhs, rhs](double out_grad) -> void { rhs.GradRef() += lhs * out_grad; });
   return out;
 }
 
@@ -132,30 +123,21 @@ auto operator*(const Value& lhs, double rhs) -> Value {
 auto operator-(double lhs, const Value& rhs) -> Value {
   Value out(lhs - rhs.Data(), {rhs});
   out.m_state_->op_ = Operation::kSubtract;
-  std::function<void(double)> backward = [rhs](double out_grad) -> void {
-    rhs.GradRef() += -1 * out_grad;
-  };
-  out.SetBackward(backward);
+  out.SetBackward([rhs](double out_grad) -> void { rhs.GradRef() += -1 * out_grad; });
   return out;
 }
 
 auto operator-(const Value& lhs, double rhs) -> Value {
   Value out(lhs.Data() - rhs, {lhs});
   out.m_state_->op_ = Operation::kSubtract;
-  std::function<void(double)> backward = [lhs](double out_grad) -> void {
-    lhs.GradRef() += out_grad;
-  };
-  out.SetBackward(backward);
+  out.SetBackward([lhs](double out_grad) -> void { lhs.GradRef() += out_grad; });
   return out;
 }
 
 auto operator+(double lhs, const Value& rhs) -> Value {
   Value out(lhs + rhs.Data(), {rhs});
   out.m_state_->op_ = Operation::kAdd;
-  std::function<void(double)> backward = [rhs](double out_grad) -> void {
-    rhs.GradRef() += out_grad;
-  };
-  out.SetBackward(backward);
+  out.SetBackward([rhs](double out_grad) -> void { rhs.GradRef() += out_grad; });
   return out;
 }
 
